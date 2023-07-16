b678e85de61b92563885e6dae0bdff218a53c483
kawasemium <90184566+kawasemium@users.noreply.github.com>
2023-07-16 18:22:35 +0900

    in_kaikatsuClub

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+class Word{
+    Origin;
+    Num;
+    constructor(){
+        Origin="";
+        Num=1;
+    }
+}
+
+
+function WordFall(){
+    let index=Math.floor(Math.random()*(WordList.length));
+    
+}
+
 let WordList = [
-    ["‚ ", 1],
-    ["‚¢",20]
+    //[0]"word", [1]period
+    ["a", 1950],
+    ["b",1950],
+    ["c",1950],
+    ["d",1950],
+    ["e",1950],
+    ["f",1950],
+    ["g",1950],
+    ["h",1950],
+    ["i",1950],
+    ["j",1950],
+    ["k",1950],
+    ["l",1950],
+    ["m",1950],
+    ["n",1950],
+    ["o",1950],
+    ["p",1950],
+    ["q",1950],
+    ["r",1950],
+    ["s",1950],
+    ["t",1950]
 ]
